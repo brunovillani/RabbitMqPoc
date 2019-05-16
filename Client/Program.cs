8f55a1d70b20a3751d3e4ecdc8be1715e7258481
Bruno Villani <brunovillani@gmail.com>
2019-05-16 12:44:17 -0400

    work queues

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,24 +13,32 @@ namespace Client
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "hello",
+                channel.QueueDeclare(queue: "task_queue",
                                      durable: false,
                                      exclusive: false,
                                      autoDelete: false,
                                      arguments: null);
 
-                string message = "Hello World!";
-                var body = Encoding.UTF8.GetBytes(message);
+                string message = GetMessage(args);
+                byte[] body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
 
                 channel.BasicPublish(exchange: "",
-                                     routingKey: "hello",
-                                     basicProperties: null,
+                                     routingKey: "task_queue",
+                                     basicProperties: properties,
                                      body: body);
                 Console.WriteLine(" [x] Sent {0}", message);
             }
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
         }
     }
 }
